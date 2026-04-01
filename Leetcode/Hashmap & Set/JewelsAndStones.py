01f1550757e3640177068dbbc6866885aca2cb26
Binu <binaykushwaha1244@gmail.com>
2026-04-02 00:35:47 +0530

    LC 771 Jewels and Stones solved

---
+++ Leetcode/Hashmap & Set/JewelsAndStones.py
@@ -0,0 +1,25 @@
+class Solution:
+    def numJewelsInStones(self, jewels: str, stones: str) -> int:
+        
+        
+        # # O(n*m)
+        # count = 0
+
+        # for stone in stones:
+        #     if stone in jewels:
+        #         count += 1
+        
+        # return count
+
+
+        # Hashing
+        # O(n+m)
+
+        s = set(jewels)
+
+        count = 0
+
+        for stone in stones:
+            if stone in s:
+                count += 1
+        return count 
