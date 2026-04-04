f87102679a898587b26972094a5f363420ccd4aa
Binu <binaykushwaha1244@gmail.com>
2026-04-04 23:24:10 +0530

    LC 1 Two Sum Hashmap & set solved

---
+++ Leetcode/Hashmap & Set/1_TwoSum.py
@@ -0,0 +1,30 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        # n = len(nums)
+        # for i in range(n):
+        #     for j in range(i+1,n):
+        #         if nums[i] + nums[j] == target:
+        #             return [i,j]
+
+        # n = len(nums)
+        # seen = {}
+        # for i in range(n):
+        #     current = nums[i]
+        #     need = target - current
+        #     if need in seen:
+        #         return [seen[need], i]
+
+        #     seen[current] = i
+
+        # Hashmap & Set
+        h = {}
+
+        for i in range(len(nums)):
+            h[nums[i]] = i
+        
+        for i in range(len(nums)):
+            y = target - nums[i]
+
+            if y in h and h[y] != i:
+                return [i, h[y]]
+
