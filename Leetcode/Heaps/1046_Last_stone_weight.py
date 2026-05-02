1f1114e1bc133f7d397799315d6f8631e01e1259
Binu <binaykushwaha1244@gmail.com>
2026-05-02 23:42:54 +0530

    Heap 1046 Last Stone Weight

--- Leetcode/Heaps/1046_Last_stone_weight.py
+++ Leetcode/Heaps/1046_Last_stone_weight.py
@@ -21,3 +21,27 @@ class Solution:
 
 # Time: O(n log n)
 # Space: O(1)
+
+
+
+
+
+import heapq
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        
+        stones = [-s for s in stones]
+        heapq.heapify(stones)
+
+        while len(stones) > 1:
+            y = -heapq.heappop(stones)
+            x = -heapq.heappop(stones)
+
+            if y != x:
+                heapq.heappush(stones, -(y-x))
+        
+        return -stones[0] if stones else 0
+
+# Heap Build O(n)
+# Each Operation O(logn)
+# Total Complexity O(nlogn)
